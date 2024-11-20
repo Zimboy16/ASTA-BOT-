0c07c308032a3c1c07b555a8a108a7fda3a03ac7
Malvin King <malvink003@gmail.com>
2024-11-20 09:48:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9aMDBLRb#nixx_85Xa_uy4-KHPXbrA5AZeDOq2F3wsh8_BAOOJyE",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/uxnee2.jpg",
-SUDO_NB: process.env.SUDO_NB || "263780934873",
+SESSION_ID: process.env.SESSION_ID || "Your id here",
+ALIVE_IMG: process.env.ALIVE_IMG || "alive url here",
+SUDO_NB: process.env.SUDO_NB || "254114127157",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
